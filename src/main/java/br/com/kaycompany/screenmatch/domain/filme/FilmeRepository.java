073514afb7ff86ce02b9778972c6b5480ee00bc9
kayqueln <rm550782@fiap.com.br>
2023-06-07 21:44:34 -0300

    Initial commit

---
+++ src/main/java/br/com/kaycompany/screenmatch/domain/filme/FilmeRepository.java
@@ -0,0 +1,5 @@
+package br.com.kaycompany.screenmatch.domain.filme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, Long> { }
